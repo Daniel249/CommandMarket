63347f0d0815b40c5d8a81a9b2074234ac33a791
Daniel Morillo <danielmorillo249@hotmail.com>
2022-11-12 16:50:49 -0500

    added time delay to loop

--- code_to_run.py
+++ code_to_run.py
@@ -1,5 +1,6 @@
 from set_up import initialize
 from commandmarket import Command
+import time
 
 comandos = initialize()
 print("------------------ Bienvenido ------------------")
@@ -7,6 +8,7 @@ print("Se encuentra en la terminal amarilla de uninorte")
 print("-----Si necesita ayuda use el comando help-----")
 while Command.continuar:
   command = input().split(" ", 1)
+  time.sleep(2)
   if command[0] == "help":
     if len(command) == 1 or command[1] == "":
       print("Los comandos son los siguientes:")

a9132cd2ac3f7df416bf3cf9618076e0f5a875f4
Daniel Morillo <danielmorillo249@hotmail.com>
2022-11-12 16:49:02 -0500

    added comments to classes

--- commandmarket.py
+++ commandmarket.py
@@ -1,11 +1,18 @@
 from typing import List, Set, Dict, Tuple, Optional
+import time
 
 class Product:
+  """
+  Clase de producto con su nombre y precio para realizar la compra
+  """
   def __init__(self, name:str, price:int) -> None:
     self.name = name
     self.price = price
 
 class Seller:
+  """
+  Clase vendedor para navegacion por nombre y contener todos los productos
+  """
   Sellers = []
   def __init__(self, name:str, location:str, products:List[Product]) -> None:
     self.name = name
@@ -14,6 +21,9 @@ class Seller:
     Seller.Sellers.append(self)
 
 class Category:
+  """
+  Clase de categorias de productos para navegacion
+  """
   Categories = []
   def __init__(self, name:str, sellers:List[Seller]) -> None:
     self.name = name
@@ -21,6 +31,9 @@ class Category:
     Category.Categories.append(self)
 
 class Command:
+  """
+  Clase que contiene la lista de comandos y sus funciones
+  """
   continuar = True
   def __init__(self, name:str, help:str) -> None:
     self.name = name
@@ -29,10 +42,13 @@ class Command:
   def metodo(command:List[str]):
     if command[0] == "exit":
       print("La aplicacion se cerrara pronto. Gracias por hacer uso de esta")
-      print("Atte. Daniel Morillo y Mateo Suarez")
+      print("\nAtte. Daniel Morillo y Mateo Suarez")
+      time.sleep(3)
+
       Command.continuar = False
     elif command[0] == "project":
-      print("Este proyecto es realizado para la clase OOP")
+      print("Este proyecto es realizado para la clase OOP de Jose D. Posada")
+      print("Realizado por Daniel Morillo y Mateo Suarez")
     elif command[0] == "catalog":
       if len(command) == 1 or command[1] == "":
         for cat in Category.Categories:
